7212feadb8b84fa8e6366e2618000de691964de6
codedrinker <gwangchunlei@gmail.com>
2020-06-20 12:01:20 +0800

    添加备注

--- src/main/java/com/github/developer/weapons/model/official/OfficialUserInfo.java
+++ src/main/java/com/github/developer/weapons/model/official/OfficialUserInfo.java
@@ -4,15 +4,57 @@ import lombok.Data;
 
 @Data
 public class OfficialUserInfo {
+    /**
+     * 是否订阅当前订阅号
+     *
+     * @return
+     */
+    private boolean isSubscribe() {
+        return 0 != subscribe;
+    }
+
+    /**
+     * 用户是否订阅该公众号标识，值为0时，代表此用户没有关注该公众号，拉取不到其余信息。
+     */
     private Integer subscribe;
+    /**
+     * 用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
+     */
     private Integer sex;
+    /**
+     * 用户的标识，对当前公众号唯一
+     */
     private String openid;
+    /**
+     * 用户的昵称
+     */
     private String nickname;
+    /**
+     * 用户的语言，简体中文为zh_CN
+     */
     private String language;
-    private String city;
-    private String province;
+    /**
+     * 用户所在国家
+     */
     private String country;
+    /**
+     * 用户所在城市
+     */
+    private String city;
+    /**
+     * 用户所在省份
+     */
+    private String province;
+    /**
+     * 用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
+     */
     private String headimgurl;
+    /**
+     * 只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段
+     */
     private String unionid;
+    /**
+     * 公众号运营者对粉丝的备注，公众号运营者可在微信公众平台用户管理界面对粉丝添加备注
+     */
     private String remark;
 }
