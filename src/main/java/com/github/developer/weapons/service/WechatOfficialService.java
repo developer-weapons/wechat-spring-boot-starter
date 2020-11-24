68fac1a8c764de304f00d9e314dbc5a060cfca8f
codedrinker <gwangchunlei@gmail.com>
2020-11-25 01:33:18 +0800

    增加转化类型

--- src/main/java/com/github/developer/weapons/service/WechatOfficialService.java
+++ src/main/java/com/github/developer/weapons/service/WechatOfficialService.java
@@ -5,13 +5,55 @@ import com.github.developer.weapons.exception.WechatException;
 import com.github.developer.weapons.model.official.OfficialCustomMessage;
 import com.github.developer.weapons.model.official.OfficialUserInfo;
 import com.github.developer.weapons.model.official.OfficialUserQuery;
+import com.github.developer.weapons.util.XmlUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /**
  * 微信订阅号
  */
 public class WechatOfficialService extends WechatBaseService {
+
+    /**
+     * 非加密的信息流转换为map
+     *
+     * @param is
+     * @return
+     */
+    public Map<String, String> toMap(InputStream is) {
+        Map<String, String> map = new HashMap<>();
+        try {
+            SAXReader reader = new SAXReader();
+            Document doc = reader.read(is);
+            Element root = doc.getRootElement();
+            List<Element> list = root.elements();
+            for (Element e : list) {
+                map.put(e.getName(), e.getText());
+            }
+            is.close();
+            return map;
+        } catch (Exception e) {
+            throw new WechatException("toMap error with" + e.getMessage());
+        }
+    }
+
+    /**
+     * 转换构建好的类型为 String
+     *
+     * @param officialCustomMessage
+     * @return
+     */
+    public String toMsg(OfficialCustomMessage officialCustomMessage) {
+        return XmlUtils.objectToXml(officialCustomMessage);
+    }
 
     /**
      * 通过 officialUserQuery 获取用户资料
