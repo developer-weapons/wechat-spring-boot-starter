ba993c44c04b3b03fc92cdb1b02ea0468a9ccc36
codedrinker <small_artisan@126.com>
2021-11-06 22:06:21 +0800

    增加注解

--- src/main/java/com/github/developer/weapons/model/official/OfficalAccessTokenQuery.java
+++ src/main/java/com/github/developer/weapons/model/official/OfficalAccessTokenQuery.java
@@ -1,8 +1,10 @@
 package com.github.developer.weapons.model.official;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class OfficalAccessTokenQuery {
     private String appid;
     private String secret;
